d9fbbe66f1ce6d1cce6b5c9ff5ff29066a2f98ee
Diam Trejo <trejodev@Diams-MacBook-Pro.local>
2023-06-03 20:58:49 +0200

    Añado ruta events

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use( express.json() );
 //Rutas
 //TODO: auth // crear, login, renew
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events') );
 
 app.get( '*', ( req, res ) => {
     res.sendFile( __dirname + '/public/index.html' )
